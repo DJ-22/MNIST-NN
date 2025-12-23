dbe71bb7546f0354f84e5cb073198c91817dd7b5
DJ-22 <jaindaksh006@gmail.com>
2025-12-24 00:55:08 +0530

    feat(train): add training loop and gradient descent implementation

---
+++ train.py
@@ -0,0 +1,26 @@
+import numpy as np
+from model import initParam, forwProp, backProp, updParam
+from data import M
+
+
+def pred(a):
+    return np.argmax(a, axis=0)
+
+def acc(preds, y):
+    return np.sum(preds == y) / y.size
+
+def gradDesc(x, y, steps, lr):
+    w1, b1, w2, b2 = initParam()
+    
+    for i in range(steps):
+        z1, a1, z2, a2 = forwProp(w1, b1, w2, b2, x)
+        dw1, db1, dw2, db2 = backProp(w1, z1, a1, w2, z2, a2, x, y, M)
+        w1, b1, w2, b2 = updParam(w1, dw1, b1, db1, w2, dw2, b2, db2, lr)
+        
+        if i % 50 == 0:
+            preds = pred(a2)
+            accuracy = acc(preds, y)
+            
+            print(f'Step {i}, Accuracy: {(accuracy * 100):.2f}%')
+    
+    return w1, b1, w2, b2
