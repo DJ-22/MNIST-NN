5bff85a4ac81e293082c7341b38f2d7260449db7
DJ-22 <jaindaksh006@gmail.com>
2025-12-25 00:59:19 +0530

    feat(train): add checkpoint loading and periodic parameter saving

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import numpy as np
-from model import initParam, forwProp, backProp, updParam
+from model import initParam, forwProp, backProp, updParam, loadParam, saveParam
 from data import M
 
 
@@ -10,7 +10,11 @@ def acc(preds, y):
     return np.sum(preds == y) / y.size
 
 def gradDesc(x, y, steps, lr):
-    w1, b1, w2, b2 = initParam()
+    param = loadParam()
+    if param:
+        w1, b1, w2, b2 = param
+    else:
+        w1, b1, w2, b2 = initParam()
     
     for i in range(steps):
         z1, a1, z2, a2 = forwProp(w1, b1, w2, b2, x)
@@ -18,6 +22,8 @@ def gradDesc(x, y, steps, lr):
         w1, b1, w2, b2 = updParam(w1, dw1, b1, db1, w2, dw2, b2, db2, lr)
         
         if i % 50 == 0:
+            saveParam(w1, b1, w2, b2)
+            
             preds = pred(a2)
             accuracy = acc(preds, y)
             
