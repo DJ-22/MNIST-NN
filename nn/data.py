260d0751f6ba039e3358d29f4bd9d6ea872fc696
DJ-22 <jaindaksh006@gmail.com>
2025-12-26 02:24:40 +0530

    feat(data): switch to randomized 80-20 train-test split

--- nn/data.py
+++ nn/data.py
@@ -8,11 +8,13 @@ data = np.array(data)
 m, n = data.shape
 np.random.shuffle(data)
 
-test_data = data[:1000].T
+split = int(0.8 * m)
+
+test_data = data[split:].T
 test_Y = test_data[0]
 test_X = test_data[1:] / 255
 
-train_data = data[1000:].T
+train_data = data[:split].T
 train_Y = train_data[0]
 train_X = train_data[1:] / 255
 M = train_Y.size
