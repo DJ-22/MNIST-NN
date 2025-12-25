5c76ec42e4878ac518f4d00aa662712c00142a63
DJ-22 <jaindaksh006@gmail.com>
2025-12-26 02:27:18 +0530

    feat(main): add digit drawing option

--- nn/main.py
+++ nn/main.py
@@ -2,12 +2,14 @@ from data import train_X, train_Y, test_X, test_Y
 from model import loadParam
 from train import gradDesc
 from test import testNN, plotAcc
+from ui import DigitUI
 
 
 if __name__ == '__main__':
     print("1: Train the Neural Network")
     print("2: Test the Neural Network")
-    choice = input("\nEnter your choice (1/2): ")
+    print("3: Draw a Digit")
+    choice = input("\nEnter your choice (1/2/3): ")
     
     if choice == '1':
         STEPS = int(input("Enter number of training steps: "))
@@ -26,5 +28,7 @@ if __name__ == '__main__':
         print("\nEvaluating on Test Set:\n")
         testNN(W1, B1, W2, B2, test_X, test_Y)
         plotAcc(W1, B1, W2, B2, test_X, test_Y)
+    elif choice == '3':
+        DigitUI()
     else:
         print("Invalid choice. Please enter 1 or 2.")
